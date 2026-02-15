69934c7beb64255c7ec4641613109df506e1c82c
Nikita Elfimov <nelfimov@mail.ru>
2026-02-15 20:11:52 +0300

    feat(common): use new translation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,70 @@
+export const languages = {
+  en: 'English',
+  ru: 'Russian',
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'meta.title': 'Portfolio: Nikita Elfimov, Web Dev',
+    'meta.applicationName': 'Nikita Elfimov Web Dev portfolio',
+
+    'nav.me': 'me',
+    'nav.about': 'about',
+    'nav.projects': 'projects',
+    'nav.github': 'github',
+    'nav.language': 'ru',
+    'nav.language-label': 'Switch language to Russian',
+    'nav.menuAlt': 'menu',
+
+    'hero.firstName': 'Nikita',
+    'hero.lastName': 'Elfimov',
+    'hero.roleFirst': 'Web',
+    'hero.roleSecond': 'Developer',
+    'hero.imageAlt': 'profile',
+
+    'about.title': 'About me',
+
+    'projects.title': 'My projects',
+    'projects.ecommerce': 'Ecommerce shop',
+    'projects.ecommerce2': 'Ecommerce shop 2',
+    'projects.waldo': 'Where is Waldo game',
+    'projects.battleship': 'Battleship game',
+    'projects.todo': 'Todo app',
+    'projects.memory': 'Memory card game',
+    'projects.tictactoe': 'Tic-tac-toe game',
+
+    'footer.builtWith': 'Built with ',
+  },
+  ru: {
+    'meta.title': 'Портфолио: Никита Елфимов, веб-разработчик',
+    'meta.applicationName': 'Портфолио Никиты Елфимова',
+
+    'nav.me': 'обо мне',
+    'nav.about': 'опыт',
+    'nav.projects': 'проекты',
+    'nav.language': 'en',
+    'nav.language-label': 'Переключить язык на английский',
+    'nav.menuAlt': 'меню',
+
+    'hero.firstName': 'Никита',
+    'hero.lastName': 'Елфимов',
+    'hero.roleFirst': 'Веб',
+    'hero.roleSecond': 'Разработчик',
+    'hero.imageAlt': 'профиль',
+
+    'about.title': 'Обо мне',
+
+    'projects.title': 'Мои проекты',
+    'projects.ecommerce': 'Интернет-магазин',
+    'projects.ecommerce2': 'Интернет-магазин 2',
+    'projects.waldo': 'Игра Where is Waldo',
+    'projects.battleship': 'Игра Морской бой',
+    'projects.todo': 'Todo-приложение',
+    'projects.memory': 'Игра Memory card',
+    'projects.tictactoe': 'Игра Крестики-нолики',
+
+    'footer.builtWith': 'Сделано с помощью ',
+  },
+} as const
